58c22e4e5264cb201e216be0ad7de038daa36d06
Yan Dvornitskyi <dvornitskyi@gmail.com>
2021-10-28 13:38:05 +0300

    add my-app-react

---
+++ my-app-react/src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button} from '../Button/Button';
+import {Input} from '../Input/Input';
+
+function App() {
+  return (
+    <div className="App">
+        <Button text = "Click me" />
+        <Input />
+    </div>
+  );
+}
+
+export default App;
